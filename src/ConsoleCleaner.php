e908f51e12dd0d06d3b910f389fcae2da1597be1
Petr Popov <air-dev@yandex.ru>
2022-02-10 22:55:39 +0400

    Small refactoring

--- src/ConsoleCleaner.php
+++ src/ConsoleCleaner.php
@@ -4,10 +4,22 @@ namespace AirPetr;
 
 class ConsoleCleaner
 {
+    /**
+     * @param int $numberOfLines
+     * @return void
+     */
     public function clean(int $numberOfLines): void
     {
-        echo chr(27) . "[0G";
-        echo chr(27) . "[" . $numberOfLines . "A";
-        echo chr(27) . "[0J";
+        echo $this->getEscChr() . "[0G";
+        echo $this->getEscChr() . "[" . $numberOfLines . "A";
+        echo $this->getEscChr() . "[0J";
+    }
+
+    /**
+     * @return string
+     */
+    public function getEscChr(): string
+    {
+        return chr(27);
     }
 }
